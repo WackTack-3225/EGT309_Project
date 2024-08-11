440ec343286d74ec26b9be6f5b26574786b933df
edyson <edysontan2005@gmail.com>
2024-08-11 12:08:03 +0800

    config

--- Pod_retriever/retrieve.py
+++ Pod_retriever/retrieve.py
@@ -1,10 +1,19 @@
 import time
 from datetime import datetime
+import os
+
+# Retrieve environment variables else default value
+data_path = os.getenv('DATA_PATH', '/mnt/data')
+model_path = os.getenv('MODEL_PATH', '/mnt/save-model')
+volume_mount_path = os.getenv('VOLUME_MOUNT_PATH', '/mnt')
+
+heartbeat_file_path = os.path.join(volume_mount_path, 'test.txt')
+
 
 while True:
     try:
         # Write to a heartbeat file every 5 seconds
-        with open("/mnt/data/heartbeat.txt", "a") as heartbeat_file:
+        with open(heartbeat_file_path, "a") as heartbeat_file:
             heartbeat_file.write(f"Heartbeat at {datetime.now()}\n")
         print("Heartbeat written.")
     except Exception as e:
