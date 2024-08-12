925a1e6d76fb21ff823ae5bc30c5ee82aa938913
edyson <edysontan2005@gmail.com>
2024-08-12 14:42:39 +0800

    Inference logging

--- Pod_retriever/retrieve.py
+++ Pod_retriever/retrieve.py
@@ -24,21 +24,18 @@ models_path = os.path.join(directpath, 'model_name.h5')  # Assuming your image f
 
 
 
-# Function to write logs to a file ensuring path exists
-def log_message(message, log_file='path_check.log'):
-    with open(log_file, "a") as log:
-        log.write(f"{datetime.now()}: {message}\n")
-# Check if paths exist and write to log
-if os.path.exists(image_path):
-    log_message(f"Image path exists: {image_path}")
-else:
-    log_message(f"Image path does NOT exist: {image_path}")
+# Function to return a log file object, ensuring path exists
+def get_log_file():
+    log_file_path = os.path.join(volume_mount_path, "inference.log")  # Use the environment variable    
+    log = open(log_file_path, "a")    
+    return log
 
-# Check if paths exist and write to log
-if os.path.exists(models_path):
-    log_message(f"Image path exists: {models_path}")
-else:
-    log_message(f"Image path does NOT exist: {models_path}")
+# Starting logfile
+log = get_log_file()
+log.write("--------------------------\n")
+log.write("Logging for inference pod\n")
+log.write(f"Log start time: {datetime.now()}\n")
+log.write("--------------------------\n")
 
 
 
@@ -59,20 +56,31 @@ def load_model_from_path(models_path):
     try:
         model = load_model(models_path)
         print("Model loaded successfully.")
+        log.write(f"{datetime.now()}: Model Loaded Successfully\n")
         return model
     except Exception as e:
         print(f"Failed to load the model. Error: {e}")
-model = load_model_from_path(models_path)
+        log.write(f"{datetime.now()}: Failed to load the model. Error: {e}\n")
 
 
+# Enusring model is loaded
+try:
+    model = load_model_from_path(models_path)
+except:
+    print(f"Failed to load the model")
+
+# Ensuring image is loaded
 try:
     img = utils.load_img(image_path, target_size=(64, 64))
     img_tensor = utils.img_to_array(img)
     img_tensor = np.expand_dims(img_tensor, axis=0)
     img_tensor = img_tensor / 255.0
     print("Image loaded and preprocessed successfully.")
+    log.write(f"{datetime.now()}: Image Loaded and preprocessed sucessfully\n")
 except Exception as e:
     print(f"Failed to load or preprocess the image. Error: {e}")
+    log.write(f"{datetime.now()}: Failed to load or preprocess the image. Error: {e}\n")
+
 
 
 def get_predicted_class(prediction):
@@ -91,18 +99,32 @@ def get_predicted_class(prediction):
         output = np.argmax(prediction)
 
         # Map the index to the class label
-        predicted_class = class_labels.get(output, "Unknown class")        
+        predicted_class = class_labels.get(output, "Unknown class") 
+        print(f"Successfully got predicted class")
+        log.write(f"{datetime.now()}: Successfully got predicted class\n")
         return predicted_class
 
     except Exception as e:
         print(f"Failed to get predicted class. Error: {e}")
+        log.write(f"{datetime.now()}: Failed to get predicted class. Error: {e}\n")
 
 
-# Prediction
-prediction = model.predict(img_tensor)
-output = get_predicted_class(prediction)
-print(f"Predicted class: {output}")
+# Ensure Prediction
+try:
+    prediction = model.predict(img_tensor)
+    output = get_predicted_class(prediction)
+    print(f"Predicted class: {output}")
+    log.write(f"{datetime.now()}: Model Predicted Successfully\n")
+except Exception as e:
+    print(f"Failed to predict image. Error: {e}")
+    log.write(f"{datetime.now()}: Failed to predict image. Error: {e}\n")
 
+# Closing logs
+log.write("--------------------------\n")
+log.write("Logging for inference pod\n")
+log.write(f"Log end time: {datetime.now()}\n")
+log.write("--------------------------\n\n")
+log.close()
 
 while True:
     time.sleep(5)  # Sleep for 5 seconds
