efd60dcff16631baafb093b9fdb57548ea1948fd
WackTack-3225 <businesszheng@yahoo.com>
2024-08-15 22:40:09 +0800

    mainly completed run.sh & updated main deployment variables

--- run.sh
+++ run.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
-
-# Handle minikube processess (assuming is downloaded)
 minikube delete
+
 minikube start --cpus=4 memory=4096
+
 eval $(minikube docker-env) 
 
 # Run the consolidated deployment file on start-up
-kubectl apply -f deployment.yml
+kubectl apply -f main_deployment.yml
 
 # Function to display the menu
 function show_menu() {
-    echo "1) Get pods status"
-    echo "2) Get url to application"
-    echo "3) Exit"
+    echo "1) Pod Overwatch"
+    echo "2) Get pods status"
+    echo "3) Get url to application"
+    echo "4) Exit"
     echo -n "Please enter your choice: "
 }
 
 # Function to perform Task 1
 function task1() {
+    echo "The pods take a while to spin up, please have patience"
+    echo "enter ctrl+c to exit overwatch mode"
     kubectl get pods -n ml-app -w
 }
 
 # Function to perform Task 2
 function task2() {
+    kubectl get pods -n ml-app
+}
+
+# Function to perform Task 3
+function task3() {
     # Attempt to retrieve the service URL
-    SERVICE_URL=$(minikube service flask-app-service -n ml-app --url)
-    
-    if [[ $SERVICE_URL =~ http:// ]]; then
-        echo "Service URL: $SERVICE_URL"
-    else
-        echo "No valid URL returned. Please try again."
-    fi
+    minikube service flask-app-service -n ml-app --url
 }
 
 # Main logic
@@ -41,7 +43,8 @@ do
     case "$CHOICE" in
         1) task1 ;;
         2) task2 ;;
-        3) echo "Exiting..."
+        3) task3 ;;
+        4) echo "Exiting..."
            exit ;;
         *) echo "Invalid choice. Please select 1, 2, 3"
            echo "Press any key to continue..."
